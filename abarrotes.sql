874d1590d1a301862d1366389d59dbf0b5d1064b
JPenaloza1 <100963782+JPenaloza1@users.noreply.github.com>
2022-07-12 09:44:44 -0500

    Cambios en la tabla productos

--- abarrotes.sql
+++ abarrotes.sql
@@ -3,7 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
+<<<<<<< HEAD
+-- Tiempo de generación: 12-07-2022 a las 08:48:30
+=======
 -- Tiempo de generación: 11-07-2022 a las 16:20:25
+>>>>>>> b6a83f44f5630a0895f21b00486cb088e07e95cb
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -24,6 +28,78 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+<<<<<<< HEAD
+-- Estructura de tabla para la tabla `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2022_07_09_210359_create_productos_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+=======
+>>>>>>> b6a83f44f5630a0895f21b00486cb088e07e95cb
 -- Estructura de tabla para la tabla `productos`
 --
 
@@ -32,9 +108,15 @@ CREATE TABLE `productos` (
   `producto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `caducidad` date NOT NULL,
   `cantidad` int(11) NOT NULL,
+<<<<<<< HEAD
+  `precio` decimal(8,2) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+=======
   `precio` double NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL
+>>>>>>> b6a83f44f5630a0895f21b00486cb088e07e95cb
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -42,6 +124,37 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `producto`, `caducidad`, `cantidad`, `precio`, `created_at`, `updated_at`) VALUES
+<<<<<<< HEAD
+(1, 'Barritas Fresa', '2022-10-05', 3, '10.50', '2022-07-12 06:43:41', '2022-07-12 11:44:18'),
+(2, 'Coca-Cola 600ml', '2024-11-15', 10, '16.50', '2022-07-12 06:45:16', '2022-07-12 06:45:16'),
+(4, 'Chetos Flaming Hot', '2023-04-13', 7, '11.00', '2022-07-12 06:46:38', '2022-07-12 06:46:38'),
+(5, 'Doritos Pizzerola', '2022-12-12', 6, '15.35', '2022-07-12 06:47:09', '2022-07-12 06:47:09'),
+(6, 'Takis Fuego', '2023-03-12', 4, '17.00', '2022-07-12 06:47:34', '2022-07-12 06:47:34');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Jonathan Alexis', 'jonathanalex20@outlook.com', NULL, '$2y$10$W9zTI2oJxJbUpX99nf3npOAwqROH3f7cUH041wsX.OzbuP4ZH7HGa', NULL, '2022-07-12 11:43:13', '2022-07-12 11:43:13');
+=======
 (7, 'Boing Uva 550ml', '2022-12-23', 5, 16.5, '2022-07-10 05:07:38', '2022-07-11 10:39:28'),
 (8, 'Sabritas Adobadas 80gr', '2023-04-10', 0, 16, '2022-07-10 05:16:54', '2022-07-10 11:01:52'),
 (11, 'Barritas piña 35gr', '2022-07-21', 8, 10, '2022-07-10 05:28:15', NULL),
@@ -56,26 +169,98 @@ INSERT INTO `productos` (`id`, `producto`, `caducidad`, `cantidad`, `precio`, `c
 (23, 'Doritos Pizzerola', '2022-12-31', 4, 15, '2022-07-11 05:20:05', NULL),
 (24, 'Doritos Nacho', '2022-10-14', 3, 14, '2022-07-11 05:21:40', NULL),
 (26, 'Chetos Bolitas', '2022-09-12', 4, 10, '2022-07-11 05:29:33', NULL);
+>>>>>>> b6a83f44f5630a0895f21b00486cb088e07e95cb
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+<<<<<<< HEAD
+-- Indices de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+=======
+>>>>>>> b6a83f44f5630a0895f21b00486cb088e07e95cb
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`);
 
 --
+<<<<<<< HEAD
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+=======
+>>>>>>> b6a83f44f5630a0895f21b00486cb088e07e95cb
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+<<<<<<< HEAD
+-- AUTO_INCREMENT de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+=======
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+>>>>>>> b6a83f44f5630a0895f21b00486cb088e07e95cb
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
